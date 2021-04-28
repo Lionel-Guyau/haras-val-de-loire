36999be86a0b00549b3ba83b7ef67dab7b6c3dcf
Pj-Touraille <pierrejean.touraille.jobs@gmail.com>
2021-04-28 19:53:39 +0200

    récupération de la connexion avec SESSION depuis master

--- src/Model/HomeManager.php
+++ src/Model/HomeManager.php
@@ -24,30 +24,18 @@ class HomeManager extends AbstractManager
     public function selectLastEvents()
     {
         $query =
-            "SELECT 'cours' AS type, 
-                lesson.id, 
-                lesson.name, 
-                lesson.start_at, 
-                COUNT(customer_lesson.lesson_id) AS nb_register, 
-                lesson.capacity
-            FROM (customer_lesson
-            INNER JOIN lesson ON customer_lesson.lesson_id = lesson.id)
-            WHERE lesson.is_active=1 AND customer_lesson.register=1 AND lesson.start_at >= now()
-            GROUP BY customer_lesson.lesson_id 
-        
-        UNION 
-        
-            SELECT 'stage' AS type, 
-                course.id, 
-                course.name, 
-                course.start_at, 
-                COUNT(customer_course.course_id) AS nb_register, course.capacity
-            FROM (customer_course
-            INNER JOIN course ON customer_course.course_id = course.id)
-            WHERE course.is_active=1 AND customer_course.register=1 AND course.start_at >= now()
-            GROUP BY customer_course.course_id
-        
-        ORDER BY start_at ASC LIMIT 3";
+            "SELECT activity.id as id,
+                activity.name AS name, 
+                activity.type AS type,
+                planning.start_at AS startingDate,
+                COUNT(customer_planning.customer_id) AS nb_register,
+                activity.capacity AS capacity
+            FROM (planning
+                INNER JOIN activity ON planning.activity_id = activity.id
+                INNER JOIN customer_planning ON planning.id = customer_planning.planning_id)
+            WHERE customer_planning.register=1 AND planning.start_at >= now()
+            GROUP BY planning.id
+            ORDER BY startingDate ASC LIMIT 3";
 
         return $this->pdo->query($query)->fetchAll();
     }
