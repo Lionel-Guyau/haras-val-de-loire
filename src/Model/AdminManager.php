e25216d527d339db4e47222b6ba93ac82fd993fd
Pj-Touraille <pierrejean.touraille.jobs@gmail.com>
2021-04-28 19:53:39 +0200

    récupération de la connexion avec SESSION depuis master

--- src/Model/AdminManager.php
+++ src/Model/AdminManager.php
@@ -8,15 +8,15 @@ use PDO;
 class AdminManager extends AbstractManager
 {
 
-    public const TABLE = 'news';
-
+    public const TABLE_NEWS = 'news';
+    public const TABLE_ACTIVITY = 'activity';
 
     /**
      * Select last news in database
      */
     public function selectNews()
     {
-        $query = "SELECT * FROM " . static::TABLE . " ORDER BY news_date DESC";
+        $query = "SELECT * FROM " . static::TABLE_NEWS . " ORDER BY news_date DESC";
 
         return $this->pdo->query($query)->fetchAll(PDO::FETCH_ASSOC);
     }
@@ -28,7 +28,7 @@ class AdminManager extends AbstractManager
     public function insertNews(string $item)
     {
         $query = $this->pdo->prepare("
-        INSERT INTO " . static::TABLE . " (`description`,`news_date`) 
+        INSERT INTO " . static::TABLE_NEWS . " (`description`,`news_date`) 
         VALUES (:description, NOW())");
         $query->bindValue(':description', $item);
 
@@ -40,7 +40,7 @@ class AdminManager extends AbstractManager
      */
     public function deleteNews(int $id): void
     {
-        $query = $this->pdo->prepare("DELETE FROM " . static::TABLE . " WHERE id = :id");
+        $query = $this->pdo->prepare("DELETE FROM " . static::TABLE_NEWS . " WHERE news.id = :id");
         $query->bindValue(':id', $id, \PDO::PARAM_INT);
 
         $query->execute();
@@ -51,10 +51,34 @@ class AdminManager extends AbstractManager
      */
     public function updateNews(int $id, string $description): void
     {
-        $query = $this->pdo->prepare("UPDATE " . static::TABLE . " 
-        SET description = :description, news_date = now() WHERE id = :id");
+        $query = $this->pdo->prepare("UPDATE " . static::TABLE_NEWS . " 
+                SET description = :description, news_date = now() WHERE id = :id");
         $query->bindValue(':id', $id, \PDO::PARAM_INT);
         $query->bindValue(':description', $description);
+        $query->execute();
+    }
+
+
+    /**
+     * Select Activity in database
+     */
+    public function selectActivity()
+    {
+        $query = "SELECT * FROM " . static::TABLE_ACTIVITY . " ORDER BY name ASC";
+
+        return $this->pdo->query($query)->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    /**
+     * Insert Activity in database
+     */
+    public function insertActivity(string $name)
+    {
+        $query = $this->pdo->prepare("
+        INSERT INTO " . static::TABLE_ACTIVITY . " (`name`,`start_at`) 
+        VALUES (:start_at, NOW())");
+        $query->bindValue(':name', $name);
+
         $query->execute();
     }
 }
