ee3d41f63732ff2cef525d4be6102c1b86056eaf
lionel-guyau <lguyau@gmail.com>
2021-04-28 11:06:26 +0200

    US19-Securite-Admin-Session

---
+++ src/Controller/LogoutController.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: aurelwcs
+ * Date: 08/04/19
+ * Time: 18:40
+ */
+
+namespace App\Controller;
+
+class LogoutController extends AbstractController
+{
+    /**
+     * Display login page
+     *
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+
+    public function index()
+    {
+        // On supprime la session
+        session_destroy();
+        // On redirige vers la racine du site
+        header('Location: /');
+    }
+}
---
+++ src/Service/AuthService.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Service;
+
+class AuthService
+{
+
+    public function __construct()
+    {
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+            session_regenerate_id();
+        }
+    }
+
+    public function checkSession()
+    {
+        if (!$this->isLogged()) {
+            header('Location: /login');
+            exit;
+        }
+    }
+
+    public function isLogged(): bool
+    {
+        return isset($_SESSION['user']);
+    }
+
+    public function getUser(): ?array
+    {
+        return $this->isLogged() ? $_SESSION['user'] : null;
+    }
+}
