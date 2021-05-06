0e3629533fa93064207799a0806894d83b5a56ff
Pj-Touraille <pierrejean.touraille.jobs@gmail.com>
2021-05-06 13:41:44 +0200

    ajout des images, des classes et des components

---
+++ src/Controller/Activity/ActivityDescription.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Controller\Activity;
+
+class ActivityDescription
+{
+    public const DESCRIPTION = [
+        1 =>    [
+                    "Objectifs et déroulement des séances :",
+                    "'Maman, je sais faire seul'.  À poney, c'est l'enfant qui joue le coup de la séparation 
+                    avec son parent, c'est lui qui le lâche. L'activité poney aide à grandir.",
+                    "On commence par brosser le poney avec Papa ou Maman, on apprend à le seller pour faire 
+                    'comme un grand'.",
+                    "Puis on se rend au manège et on échauffe les petits muscles du cavalier par quelques 
+                    exercices de motricité, puis on joue sur des parcours, on déplace des objets, on remplit 
+                    des seaux, on trie les objets, et on fait des jeux pour se socialiser.",
+                    "Puis, selon la météo, vient le temps de la promenade. Pour finir, des caresses et des 
+                    récompenses pour le poney."
+                ],
+        2 =>    [
+                    "Les enfants prendront un premier contact avec les poneys en les brossant et en participant 
+                    à leur préparation.",
+                    "Ils effectueront des jeux à poney. Ils participeront aussi à des promenades à l'extérieur.",
+                    "Les cours durent 1/2 heure, pendant laquelle ils apprendront entre autre :
+                        - Les règles de sécurité,
+                        - L'entretien du poney,
+                        - La monte à poney,
+                        - Comment s'arrêter, tourner, avancer...
+                    Afin de garantir un encadrement de qualité, le nombre d’enfants est limité.",
+                    "Grâce à une méthode pédagogique axée sur le jeu et l’amusement, les enfants, au contact du 
+                    poney, vont pouvoir travailler :
+                        - La mise en confiance,
+                        - Le développement de la motricité et de la tonicité,
+                        - La coordination et la dissociation des gestes,
+                        - Le dosage des mouvements,
+                        - La maîtrise de l’équilibre, la latéralisation.",
+                    "Les enfants sont sous la responsabilité du Haras pendant 1/2 heure"
+                ],
+        3 =>    [
+                    "Pendant la première année d'équitation, l’objectif est d’arriver à se déplacer au pas et au trot 
+                    et de découvrir les premières sensations du galop et du saut d'obstacle.",
+                    "Ensuite, l'évolution des cavaliers se fait en suivant les programmes fédéraux des galops.",
+                    "Les difficultés de chacun sont prises en compte afin d’aider les cavaliers à évoluer à leur 
+                    rythme.",
+                    "Les enseignements sont basés sur une approche ludique qui permet une assimilation intuitive 
+                    dans une ambiance joyeuse et agréable.",
+                    "Les cavaliers seront encadrés par des enseignants diplomés d’état qui sauront être à l’écoute 
+                    de leurs attentes et les accompagneront dans leur évolution.",
+                    "L’objectif de la première année d'équitation est calqué sur le programme fédéral du Poney de 
+                    Bronze - Galop de Bronze selon l'âge.",
+                    "La leçon dure une heure et quart, pendant laquelle les cavaliers apprennent à panser (=brosser, 
+                    nettoyer) et seller leur monture, monter à cheval, puis ensuite desseller sa monture, lui donner 
+                    les soins nécessaires après un cours, lui donner à manger si c'est la fin de matinée ou journée, 
+                    le ramener au pré s'il n'est plus monté par la suite...",
+                    "Les enfants sont sous la responsabilité du Haras du Val de Loire pendant 1 heure et quart."
+                ],
+        4 =>    [
+                    "Le Haras du Val de Loire propose des cours spécifiquement pour adultes. Découvrez, recommencez 
+                    ou perfectionnez-vous.",
+                    "Il n’y pas d’âge pour profiter du cheval. Nous nous adaptons à vos besoins et vous proposons des 
+                    prestations différenciées.",
+                    "Les horaires de cours :",
+                    "Le mardi, jeudi et vendredi soir de 19h00 à 21h00, mais également le samedi matin.",
+                    "Il y a aussi la possibilité de rejoindre les cours adolescents aux autres horaires.",
+                    "Modalité des cours :",
+                    "Nous proposons des cours individuels au départ si besoin puis des cours collectifs ensuite.",
+                    "Vous serez encadré par des enseignants diplomés d’état qui sauront être à l’écoute de 
+                    vos attentes et vous accompagneront dans votre évolution.",
+                    "La leçon dure deux heures, pendant laquelle les cavaliers apprennent à
+                        - panser (=brosser, nettoyer) et seller leur monture pendant 1/2h.
+                        - monter à cheval pendant 1h.
+                        - desseller sa monture, lui donner les soins nécessaires après un cours, lui donner 
+                        à manger si c'est la fin de matinée ou journée, le ramener au pré s'il n'est plus monté 
+                        par la suite.... pendant 1/2h."
+                ],
+        5 =>    [
+                    "A partir du Galop 3 :",
+                    "Vous avez la possibilité de monter votre équidé préféré. Cette formule vous permet de 
+                    progresser avec le même cheval/poney tout au long de l'année et de pouvoir également être 
+                    sûr de sortir votre cheval/poney préféré en concours si vous le souhaitez.",
+                    "Vous pouvez également venir monter seul(e) sur le cheval/poney choisi, en accord avec la 
+                    direction. La monte libre se fait exclusivement dans les installations du Haras du Val de Loire, 
+                    et pendant les heures d'ouverture de la structure, afin que le cavalier puisse monter seul, 
+                    mais avec la présence d'un responsable à proximité."
+                ],
+        6 =>    [
+                    "Déroulement d'une journée type :",
+                        "- Garderie de 8h00 à 9h15 (pour ceux qui ont choisi cette option)",
+                        "- Accueil à entre 9h15 et 9h30 pour l'arrivée des cavaliers",
+                        "- Rentrée des 35 poneys aux écuries, distribution des rations de céréales.",
+                        "- Hippologie et théorie en salle ou aux écuries.",
+                        "- Préparation des poneys, pansage, brossage, sellage...",
+                        "- Monte (ludique pour les plus petits) pendant environ une heure.",
+                        "- Rangement des écuries, des selles, des filets.",
+                        "- Pique-nique & temps calme.",
+                        "- Rebelote l'après-midi (Les plus aguerris montent deux fois l'après-midi).",
+                        "- En fin de journée, chacun sort son poney au pré.",
+                        "- Goûter et temps calme, jeux, lecture...",
+                        "- Accueil entre 17h00 et 17h15 pour le départ des cavaliers.",
+                        "- Garderie entre 17h15 et 18h30 (pour ceux qui ont choisi cette option)"
+                ],
+        7 =>    [
+                    "Objectifs du Parcours Animateur Equitation :",
+                    "L’Animateur Equitation est un titre professionnel permettant de travailler en tant que salarié 
+                    dans un centre équestre pour enseigner l'équitation du galop 1 au galop 4. Il permet d’accueillir 
+                    les différents publics, il participe à  l’entretien de la structure et participe aux soins courants 
+                    ainsi qu’au travail des équidés."
+                ]
+        ];
+
+    public function getDescription(): array
+    {
+        return static::DESCRIPTION;
+    }
+}
